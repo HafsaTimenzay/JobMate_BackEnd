d8f2b081e5cdada99626453986805709c95c7d6c
Yassy30 <128840480+Yassy30@users.noreply.github.com>
2024-12-22 13:35:24 +0100

    Delete JobMatee/JobMatee directory

--- JobMatee/JobMatee/src/main/java/com/example/JobMatee/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.JobMatee.model;
-
-public enum Role {
-    CANDIDATE,
-    RECRUITER
-}
--- JobMatee/JobMatee/src/main/java/com/example/JobMatee/repository/CandidateRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.JobMatee.repository;
-
-import com.example.JobMatee.model.Candidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {}
